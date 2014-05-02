757ab5d64729188e5bc5d906d35ef80a34dc5ece
Pointy Sharp <pointysharp@gmail.com>
2014-05-02 01:11:46 +0100

    Fill in Str's new tag field with a dummy value where needed.

--- Text/TeXMath/Pandoc.hs
+++ Text/TeXMath/Pandoc.hs
@@ -39,30 +39,30 @@ expsToInlines xs = do
   return (concat res)
 
 expToInlines :: Exp -> Maybe [Inline]
-expToInlines (ENumber s) = Just [Str s]
-expToInlines (EIdentifier s) = Just [Emph [Str s]]
-expToInlines (EMathOperator s) = Just [Str s]
-expToInlines (ESymbol t s) = Just $ addSpace t (Str s)
+expToInlines (ENumber s) = Just [Str s ()]
+expToInlines (EIdentifier s) = Just [Emph [Str s ()]]
+expToInlines (EMathOperator s) = Just [Str s ()]
+expToInlines (ESymbol t s) = Just $ addSpace t (Str s ())
   where addSpace Op x = [x]
         addSpace Bin x = [medspace, x, medspace]
         addSpace Rel x = [widespace, x, widespace]
         addSpace Pun x = [x, thinspace]
         addSpace _ x = [x]
-        thinspace = Str "\x2006"
-        medspace  = Str "\x2005"
-        widespace = Str "\x2004"
+        thinspace = Str "\x2006" ()
+        medspace  = Str "\x2005" ()
+        widespace = Str "\x2004" ()
 expToInlines (EStretchy x) = expToInlines x
 expToInlines (EDelimited start end xs) = do
   xs' <- mapM expToInlines xs
-  return $ [Str start] ++ concat xs' ++ [Str end]
+  return $ [Str start ()] ++ concat xs' ++ [Str end ()]
 expToInlines (EGrouped xs) = expsToInlines xs
-expToInlines (ESpace "0.167em") = Just [Str "\x2009"]
-expToInlines (ESpace "0.222em") = Just [Str "\x2005"]
-expToInlines (ESpace "0.278em") = Just [Str "\x2004"]
-expToInlines (ESpace "0.333em") = Just [Str "\x2004"]
-expToInlines (ESpace "1em")     = Just [Str "\x2001"]
-expToInlines (ESpace "2em")     = Just [Str "\x2001\x2001"]
-expToInlines (ESpace _)         = Just [Str " "]
+expToInlines (ESpace "0.167em") = Just [Str "\x2009" ()]
+expToInlines (ESpace "0.222em") = Just [Str "\x2005" ()]
+expToInlines (ESpace "0.278em") = Just [Str "\x2004" ()]
+expToInlines (ESpace "0.333em") = Just [Str "\x2004" ()]
+expToInlines (ESpace "1em")     = Just [Str "\x2001" ()]
+expToInlines (ESpace "2em")     = Just [Str "\x2001\x2001" ()]
+expToInlines (ESpace _)         = Just [Str " " ()]
 expToInlines (EBinary _ _ _) = Nothing
 expToInlines (ESub x y) = do
   x' <- expToInlines x
@@ -80,24 +80,24 @@ expToInlines (ESubsup x y z) = do
 expToInlines (EDown x y) = expToInlines (ESub x y)
 expToInlines (EUp x y) = expToInlines (ESuper x y)
 expToInlines (EDownup x y z) = expToInlines (ESubsup x y z)
-expToInlines (EText TextNormal x) = Just [Str x]
-expToInlines (EText TextBold x) = Just [Strong [Str x]]
+expToInlines (EText TextNormal x) = Just [Str x ()]
+expToInlines (EText TextBold x) = Just [Strong [Str x ()]]
 expToInlines (EText TextMonospace x) = Just [Code nullAttr x]
-expToInlines (EText TextItalic x) = Just [Emph [Str x]]
-expToInlines (EText _ x) = Just [Str x]
+expToInlines (EText TextItalic x) = Just [Emph [Str x ()]]
+expToInlines (EText _ x) = Just [Str x ()]
 expToInlines (EOver (EGrouped [EIdentifier [c]]) (ESymbol Accent [accent])) =
     case accent of
-         '\x203E' -> Just [Emph [Str [c,'\x0304']]]  -- bar
-         '\x00B4' -> Just [Emph [Str [c,'\x0301']]]  -- acute
-         '\x0060' -> Just [Emph [Str [c,'\x0300']]]  -- grave
-         '\x02D8' -> Just [Emph [Str [c,'\x0306']]]  -- breve
-         '\x02C7' -> Just [Emph [Str [c,'\x030C']]]  -- check
-         '.'      -> Just [Emph [Str [c,'\x0307']]]  -- dot
-         '\x00B0' -> Just [Emph [Str [c,'\x030A']]]  -- ring
-         '\x20D7' -> Just [Emph [Str [c,'\x20D7']]]  -- arrow right
-         '\x20D6' -> Just [Emph [Str [c,'\x20D6']]]  -- arrow left
-         '\x005E' -> Just [Emph [Str [c,'\x0302']]]  -- hat
-         '\x0302' -> Just [Emph [Str [c,'\x0302']]]  -- hat
-         '~'      -> Just [Emph [Str [c,'\x0303']]]  -- tilde
+         '\x203E' -> Just [Emph [Str [c,'\x0304'] ()]]  -- bar
+         '\x00B4' -> Just [Emph [Str [c,'\x0301'] ()]]  -- acute
+         '\x0060' -> Just [Emph [Str [c,'\x0300'] ()]]  -- grave
+         '\x02D8' -> Just [Emph [Str [c,'\x0306'] ()]]  -- breve
+         '\x02C7' -> Just [Emph [Str [c,'\x030C'] ()]]  -- check
+         '.'      -> Just [Emph [Str [c,'\x0307'] ()]]  -- dot
+         '\x00B0' -> Just [Emph [Str [c,'\x030A'] ()]]  -- ring
+         '\x20D7' -> Just [Emph [Str [c,'\x20D7'] ()]]  -- arrow right
+         '\x20D6' -> Just [Emph [Str [c,'\x20D6'] ()]]  -- arrow left
+         '\x005E' -> Just [Emph [Str [c,'\x0302'] ()]]  -- hat
+         '\x0302' -> Just [Emph [Str [c,'\x0302'] ()]]  -- hat
+         '~'      -> Just [Emph [Str [c,'\x0303'] ()]]  -- tilde
          _        -> Nothing
 expToInlines _ = Nothing
